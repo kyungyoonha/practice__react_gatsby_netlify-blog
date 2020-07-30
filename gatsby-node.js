7c41396e2aa3829be78afdf1e88ab711db36643e
kyungyoonha <gkb10a@naver.com>
2020-07-30 18:35:04 +0900

    created team and authors page, finished application

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,8 +2,6 @@ const { slugify } = require("./src/util/utilityFunction")
 const path = require("path")
 const authors = require("./src/util/authors")
 const _ = require("lodash")
-const { templateSettings } = require("lodash")
-const { template } = require("lodash")
 
 // 각 스키마의 모든 노드가 생길때마다 실행된다.
 // 포스트를 만들때마다 title로 slug(path)를 자동적으로 만들어줌
@@ -28,6 +26,7 @@ exports.createPages = ({ actions, graphql }) => {
         tagsPage: path.resolve("src/templates/tags-page.js"),
         tagPosts: path.resolve("src/templates/tag-posts.js"),
         postList: path.resolve("src/templates/post-list.js"),
+        authorPosts: path.resolve("src/templates/author-posts.js"),
     }
 
     return graphql(`
@@ -61,7 +60,7 @@ exports.createPages = ({ actions, graphql }) => {
                     // 쿼리문 조회할대 $slug 로 받아짐
                     // $slug 와 db에 slug일치하는 데이터만 뽑음
                     slug: node.fields.slug,
-                    // find author imageHUrl form authors and pass it to the single post template
+                    // find author imageUrl form authors and pass it to the single post template
                     imageUrl: authors.find(
                         x => x.name === node.frontmatter.author
                     ).imageUrl,
@@ -124,6 +123,18 @@ exports.createPages = ({ actions, graphql }) => {
                     skip: index * postsPerPage,
                     currentPage,
                     numberOfPages,
+                },
+            })
+        })
+
+        authors.forEach(author => {
+            console.log(author.name)
+            createPage({
+                path: `/author/${slugify(author.name)}`,
+                component: templates.authorPosts,
+                context: {
+                    authorName: author.name,
+                    imageUrl: author.imageUrl,
                 },
             })
         })
--- src/pages/team.js
+++ src/pages/team.js
@@ -1,16 +1,66 @@
 import React from "react"
-
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import authors from "../util/authors"
+import { Card, CardText, CardBody, CardTitle, Button, Row } from "reactstrap"
+import JaneImage from "../images/jane.jpg"
+import MinaImage from "../images/mina.jpg"
+import { slugify } from "../util/utilityFunction"
 
 const TeamPage = () => (
-  <Layout pageTitle="Our Team">
-    <SEO title="Team" />
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus obcaecati
-    est itaque suscipit ipsum cum excepturi, ab nisi illo asperiores voluptatum
-    tenetur numquam maiores consequatur dignissimos eos nemo similique culpa a
-    modi deleniti recusandae dicta facilis dolores? Sapiente, voluptas esse.
-  </Layout>
+    <Layout pageTitle="Our Team">
+        <SEO title="Team" />
+        <Row className="mb-4">
+            <div className="col-md-3">
+                <img
+                    src={JaneImage}
+                    style={{ maxWidth: "100%" }}
+                    lat="John profile"
+                    alt=""
+                />
+            </div>
+            <div class="col-md-8">
+                <Card style={{ minHeight: "100%" }}>
+                    <CardBody>
+                        <CardTitle>{authors[0].name}</CardTitle>
+                        <CardText>{authors[0].bio}</CardText>
+                        <Button
+                            className="text-uppercase"
+                            color="primary"
+                            href={`/author/${slugify(authors[0].name)}`}
+                        >
+                            View posts
+                        </Button>
+                    </CardBody>
+                </Card>
+            </div>
+        </Row>
+        <Row className="mb-4">
+            <div className="col-md-3">
+                <img
+                    src={MinaImage}
+                    style={{ maxWidth: "100%" }}
+                    lat="John profile"
+                    alt=""
+                />
+            </div>
+            <div class="col-md-8">
+                <Card style={{ minHeight: "100%" }}>
+                    <CardBody>
+                        <CardTitle>{authors[1].name}</CardTitle>
+                        <CardText>{authors[1].bio}</CardText>
+                        <Button
+                            className="text-uppercase"
+                            color="primary"
+                            href={`/author/${slugify(authors[1].name)}`}
+                        >
+                            View posts
+                        </Button>
+                    </CardBody>
+                </Card>
+            </div>
+        </Row>
+    </Layout>
 )
 
 export default TeamPage
