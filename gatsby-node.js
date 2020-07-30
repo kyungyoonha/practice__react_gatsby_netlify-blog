2db09d073d2abb14c31323b8d4bac5fc3c7065fb
kyungyoonha <gkb10a@naver.com>
2020-07-30 13:50:40 +0900

    created all tags page

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,61 +1,96 @@
 const { slugify } = require("./src/util/utilityFunction")
 const path = require("path")
 const authors = require("./src/util/authors")
+const _ = require("lodash")
+const { templateSettings } = require("lodash")
 
 // 각 스키마의 모든 노드가 생길때마다 실행된다.
 // 포스트를 만들때마다 title로 slug(path)를 자동적으로 만들어줌
 // 매번 slug(path)를 만들어 줄 필요 없다.
 exports.onCreateNode = ({ node, actions }) => {
-  const { createNodeField } = actions
-  if (node.internal.type === "MarkdownRemark") {
-    const slugFromTitle = slugify(node.frontmatter.title)
-    createNodeField({
-      node,
-      name: "slug",
-      value: slugFromTitle,
-    })
-  }
+    const { createNodeField } = actions
+    if (node.internal.type === "MarkdownRemark") {
+        const slugFromTitle = slugify(node.frontmatter.title)
+        createNodeField({
+            node,
+            name: "slug",
+            value: slugFromTitle,
+        })
+    }
 }
 
 exports.createPages = ({ actions, graphql }) => {
-  const { createPage } = actions
-  const singlePostTemplate = path.resolve("src/templates/single-post.js")
+    const { createPage } = actions
 
-  return graphql(`
-    {
-      allMarkdownRemark {
-        edges {
-          node {
-            frontmatter {
-              author
-              tags
-            }
-            fields {
-              slug
-            }
-          }
-        }
-      }
+    const templates = {
+        singlePost: path.resolve("src/templates/single-post.js"),
+        tagsPage: path.resolve("src/templates/tags-page.js"),
     }
-  `).then(res => {
-    if (res.errors) return Promise.reject(res.errors)
 
-    const posts = res.data.allMarkdownRemark.edges
+    return graphql(`
+        {
+            allMarkdownRemark {
+                edges {
+                    node {
+                        frontmatter {
+                            author
+                            tags
+                        }
+                        fields {
+                            slug
+                        }
+                    }
+                }
+            }
+        }
+    `).then(res => {
+        if (res.errors) return Promise.reject(res.errors)
 
-    posts.forEach(({ node }) => {
-      createPage({
-        path: node.fields.slug,
-        component: singlePostTemplate,
-        context: {
-          // Passing slug for template to use get post
-          // 쿼리문 조회할대 $slug 로 받아짐
-          // $slug 와 db에 slug일치하는 데이터만 뽑음
-          slug: node.fields.slug,
-          // find author imageHUrl form authors and pass it to the single post template
-          imageUrl: authors.find(x => x.name === node.frontmatter.author)
-            .imageUrl,
-        },
-      })
+        const posts = res.data.allMarkdownRemark.edges
+
+        // Create single blog post pages
+        posts.forEach(({ node }) => {
+            createPage({
+                path: node.fields.slug,
+                component: templates.singlePost,
+                context: {
+                    // Passing slug for template to use get post
+                    // 쿼리문 조회할대 $slug 로 받아짐
+                    // $slug 와 db에 slug일치하는 데이터만 뽑음
+                    slug: node.fields.slug,
+                    // find author imageHUrl form authors and pass it to the single post template
+                    imageUrl: authors.find(
+                        x => x.name === node.frontmatter.author
+                    ).imageUrl,
+                },
+            })
+        })
+
+        // Get all tags
+        let tags = []
+        _.each(posts, edge => {
+            if (_.get(edge, "node.frontmatter.tags")) {
+                tags = tags.concat(edge.node.frontmatter.tags)
+            }
+        })
+
+        // ['design', 'code', ...]
+        // {design: 5, code: 6, ...}
+        let tagPostCounts = {}
+        tags.forEach(tag => {
+            tagPostCounts[tag] = (tagPostCounts[tag] || 0) + 1
+        })
+
+        tags = _.uniq(tags)
+
+        // Create tags page
+        createPage({
+            path: `/tags`,
+            component: templates.tagsPage,
+            context: {
+                tags,
+                tagPostCounts,
+            },
+        })
     })
-  })
 }
--- src/templates/single-post.js
+++ src/templates/single-post.js
@@ -9,144 +9,151 @@ import authors from "../util/authors"
 import { DiscussionEmbed } from "disqus-react"
 
 const singlePost = ({ data, pageContext }) => {
-  const post = data.markdownRemark.frontmatter
-  const author = authors.find(x => x.name === post.author)
+    const post = data.markdownRemark.frontmatter
+    const author = authors.find(x => x.name === post.author)
 
-  const baseUrl = "https://www.gatsbytutorial.co.kr"
+    const baseUrl = "https://www.gatsbytutorial.co.kr"
 
-  const disqusShortname = "gatsbytutorial"
-  const disqusConfig = {
-    identifier: data.markdownRemark.id,
-    title: post.title,
-    url: baseUrl + pageContext.slug,
-  }
-  console.log(disqusConfig.identifier)
-  return (
-    <Layout
-      pageTitle={post.title}
-      postAuthor={author}
-      authorImageFluid={data.file.childImageSharp.fluid}
-    >
-      <SEO title={post.title} />
-      <Card>
-        <Img
-          className="card-image-top"
-          fluid={post.image.childImageSharp.fluid}
-        />
-        <CardBody>
-          <CardSubtitle>
-            <span className="text-info">{post.date}</span> by{" "}
-            <span className="text-info">{post.author}</span>
-          </CardSubtitle>
-          <div dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }} />
-          <ul className="post-tages">
-            {post.tags.map(tag => (
-              <li key={tag}>
-                <Link to={`/tag/${slugify(tag)}`}>
-                  <Badge color="primary">{tag}</Badge>
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </CardBody>
-      </Card>
-      <h3 class="text-center">Share this post</h3>
-      <div class="text-center social-share-links">
-        <ul>
-          <li>
-            <a
-              href={
-                "https://www.facebook.com/sharer/sharer.php?u=" +
-                baseUrl +
-                pageContext.slug
-              }
-              className="facebook"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i class="fab fa-facebook-f fa-2x"></i>
-            </a>
-          </li>
-          <li>
-            <a
-              href={
-                "https://twitter.com/share?url=" +
-                baseUrl +
-                pageContext.slug +
-                "&text=" +
-                post.title +
-                "&via" +
-                "twitterHandle"
-              }
-              className="twitter"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i class="fab fa-twitter fa-2x"></i>
-            </a>
-          </li>
-          <li>
-            <a
-              href={
-                "https://plus.google.com/share?url=" +
-                baseUrl +
-                pageContext.slug
-              }
-              className="google"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i class="fab fa-google fa-2x"></i>
-            </a>
-          </li>
-          <li>
-            <a
-              href={
-                "https://www.linkedin.com/shareArticle?url=" +
-                baseUrl +
-                pageContext.slug
-              }
-              className="linkedin"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i class="fab fa-linkedin fa-2x"></i>
-            </a>
-          </li>
-        </ul>
-      </div>
-      <DiscussionEmbed shortname={disqusShortname} config={disqusConfig} />
-    </Layout>
-  )
+    const disqusShortname = "gatsbytutorial"
+    const disqusConfig = {
+        identifier: data.markdownRemark.id,
+        title: post.title,
+        url: baseUrl + pageContext.slug,
+    }
+    console.log(disqusConfig.identifier)
+    return (
+        <Layout
+            pageTitle={post.title}
+            postAuthor={author}
+            authorImageFluid={data.file.childImageSharp.fluid}
+        >
+            <SEO title={post.title} />
+            <Card>
+                <Img
+                    className="card-image-top"
+                    fluid={post.image.childImageSharp.fluid}
+                />
+                <CardBody>
+                    <CardSubtitle>
+                        <span className="text-info">{post.date}</span> by{" "}
+                        <span className="text-info">{post.author}</span>
+                    </CardSubtitle>
+                    <div
+                        dangerouslySetInnerHTML={{
+                            __html: data.markdownRemark.html,
+                        }}
+                    />
+                    <ul className="post-tages">
+                        {post.tags.map(tag => (
+                            <li key={tag}>
+                                <Link to={`/tag/${slugify(tag)}`}>
+                                    <Badge color="primary">{tag}</Badge>
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                </CardBody>
+            </Card>
+            <h3 class="text-center">Share this post</h3>
+            <div class="text-center social-share-links">
+                <ul>
+                    <li>
+                        <a
+                            href={
+                                "https://www.facebook.com/sharer/sharer.php?u=" +
+                                baseUrl +
+                                pageContext.slug
+                            }
+                            className="facebook"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <i class="fab fa-facebook-f fa-2x"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href={
+                                "https://twitter.com/share?url=" +
+                                baseUrl +
+                                pageContext.slug +
+                                "&text=" +
+                                post.title +
+                                "&via" +
+                                "twitterHandle"
+                            }
+                            className="twitter"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <i class="fab fa-twitter fa-2x"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href={
+                                "https://plus.google.com/share?url=" +
+                                baseUrl +
+                                pageContext.slug
+                            }
+                            className="google"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <i class="fab fa-google fa-2x"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href={
+                                "https://www.linkedin.com/shareArticle?url=" +
+                                baseUrl +
+                                pageContext.slug
+                            }
+                            className="linkedin"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <i class="fab fa-linkedin fa-2x"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <DiscussionEmbed
+                shortname={disqusShortname}
+                config={disqusConfig}
+            />
+        </Layout>
+    )
 }
 
 export const postQuery = graphql`
-  query blogPostBySlug($slug: String!, $imageUrl: String) {
-    markdownRemark(fields: { slug: { eq: $slug } }) {
-      id
-      html
-      frontmatter {
-        title
-        author
-        date(formatString: "MMM Do YYYY")
-        tags
-        image {
-          childImageSharp {
-            fluid(maxWidth: 700) {
-              ...GatsbyImageSharpFluid
+    query blogPostBySlug($slug: String!, $imageUrl: String) {
+        markdownRemark(fields: { slug: { eq: $slug } }) {
+            id
+            html
+            frontmatter {
+                title
+                author
+                date(formatString: "MMM Do YYYY")
+                tags
+                image {
+                    childImageSharp {
+                        fluid(maxWidth: 700) {
+                            ...GatsbyImageSharpFluid
+                        }
+                    }
+                }
             }
-          }
         }
-      }
-    }
-    file(relativePath: { eq: $imageUrl }) {
-      childImageSharp {
-        fluid(maxWidth: 300) {
-          ...GatsbyImageSharpFluid
+        file(relativePath: { eq: $imageUrl }) {
+            childImageSharp {
+                fluid(maxWidth: 300) {
+                    ...GatsbyImageSharpFluid
+                }
+            }
         }
-      }
     }
-  }
 `
 
 export default singlePost
